aa758a463c00da6a1f695a50338eee928adc3e6a
FancyKillerPanda <iyersahil12@gmail.com>
2020-09-08 11:15:54 +1000

    Project: Booting from an ISO image.

--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,17 @@
 #  ===== Date Created: 03 September, 2020 ===== 
 
 prjRoot=..
+binDir=$prjRoot/bin
 
-mkdir -p $prjRoot/bin/
-nasm -i $prjRoot/src/boot/ -f bin $prjRoot/src/boot/bootPandaOS.asm -o $prjRoot/bin/bootPandaOS.bin
-nasm -i $prjRoot/src/boot/ -f bin $prjRoot/src/boot/loadPandaOS.asm -o $prjRoot/bin/stage2.bin
+echo "Building binaries..."
+mkdir -p $binDir/
+nasm -i $prjRoot/src/boot/ -f bin $prjRoot/src/boot/bootPandaOS.asm -o $binDir/bootPandaOS.bin
+nasm -i $prjRoot/src/boot/ -f bin $prjRoot/src/boot/loadPandaOS.asm -o $binDir/2ndstage.bin
+
+echo "Building floppies..."
+dd if=/dev/zero of=$binDir/bootPandaOS.flp bs=1024 count=1440
+dd if=$binDir/bootPandaOS.bin of=$binDir/bootPandaOS.flp seek=0 count=1 conv=notrunc
+
+echo "Building ISO image..."
+genisoimage -V "PandaVolume" -input-charset iso8859-1 -o $binDir/pandaOS.iso \
+			-b bootPandaOS.flp -hide bootPandaOS.flp $binDir
--- tools/run.bat
+++ tools/run.bat
@@ -8,7 +8,7 @@ if not exist %prjRoot%\bin\ (
 	echo There is nothing to run...
 ) else (
 	cd %prjRoot%\bin\
-	qemu-system-x86_64 --drive format=raw,file=bootPandaOS.bin
+	qemu-system-x86_64 -cdrom pandaOS.iso
 	cd %startingDirectory%
 )
 
