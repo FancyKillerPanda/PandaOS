0babc9c2c9b5d5790367b292b5c81d0aa32c05b7
FancyKillerPanda <iyersahil12@gmail.com>
2020-09-12 21:58:14 +1000

    Project: Better build and run scripts.

--- tools/build.sh
+++ tools/build.sh
@@ -1,34 +1,61 @@
 #!/bin/bash
 #  ===== Date Created: 03 September, 2020 ===== 
 
+RED="\033[0;31m"
+GREEN="\033[0;92m"
+BLUE="\033[0;36m"
+
+DEFAULT_COLOUR="\033[0m"
+
+# Argument one is the colour (ANSI escape sequence), argument two is
+# the string to echo
+print()
+{
+	echo -e "$1$2${DEFAULT_COLOUR}"
+}
+
+# Exits the script because of an error
+exit_on_error()
+{
+	print $RED "\nBuild failed!\n"
+	popd
+	exit 1
+}
+
+# Directories
 prjRoot=..
 srcDir=$prjRoot/src
+bootDir=$srcDir/boot
+kernelDir=$srcDir/kernel
+
+# Flags
+kernelCompileFlags="-ffreestanding -nostdinc -nostdlib -o pKernelA.bin -target i386-pc-none-elf"
+kernelLinkFlags="-Wl,--oformat=binary,-T$prjRoot/tools/kernelLinker.ld"
+kernelFiles="kernel_entry.o $kernelDir/kernel.c"
 
 mkdir -p $prjRoot/bin
 pushd $prjRoot/bin > /dev/null
 
-echo "Cleaning..."
+print $BLUE "Cleaning..."
 rm *.bin *.img *.iso *.o 2> /dev/null
 
-echo
-echo "Building binaries..."
-nasm -i $srcDir/boot $srcDir/boot/bootPandaOS.asm -o bootPandaOS.bin
-nasm -i $srcDir/boot $srcDir/boot/loadPandaOS.asm -o pkLoader.bin
-nasm -felf32 -i $srcDir/boot $srcDir/kernel/kernel_entry.asm -o kernel_entry.o
-clang -ffreestanding -nostdinc -nostdlib -o pKernelA.bin -target i386-pc-none-elf -Wl,--oformat=binary,-T$prjRoot/tools/kernelLinker.ld kernel_entry.o $srcDir/kernel/kernel.c
+print $BLUE "\nBuilding binaries..."
+if ! nasm -i $bootDir $bootDir/bootPandaOS.asm -o bootPandaOS.bin; then exit_on_error; fi
+if ! nasm -i $bootDir $bootDir/loadPandaOS.asm -o pkLoader.bin; then exit_on_error; fi
+if ! nasm -felf32 $kernelDir/kernel_entry.asm -o kernel_entry.o; then exit_on_error; fi
+if ! clang $kernelCompileFlags $kernelLinkFlags $kernelFiles; then exit_on_error; fi
 
-echo
-echo "Building floppies..."
-../tools/ffc_linux -b bootPandaOS.bin \
-				   -s pkLoader.bin pKernelA.bin \
-				   -o pandaFloppy.img \
-				   --ls-fat
+print $BLUE "\nBuilding floppies..."
+if ! ../tools/ffc_linux -b bootPandaOS.bin \
+						-s pkLoader.bin pKernelA.bin \
+						-o pandaFloppy.img \
+						--ls-fat \
+						; then exit_on_error; fi
 
-echo
-echo "Building ISO image..."
-genisoimage -V "PandaVolume" -input-charset iso8859-1 -o pandaOS.iso -b pandaFloppy.img .
+print $BLUE "\nBuilding ISO image..."
+if ! genisoimage -V "PandaVolume" -input-charset iso8859-1 -o pandaOS.iso -b pandaFloppy.img .; then exit_on_error; fi
 
-echo
-echo "Done!"
+print $GREEN "\nBuild succeeded!\n"
 
 popd > /dev/null
+exit 0
--- tools/run.bat
+++ tools/run.bat
@@ -3,12 +3,18 @@ rem  ===== Date Created: 03 September, 2020 =====
 
 set prjRoot=..
 set startingDirectory=%CD%
+set debug=false
+
+if "%~1" == "-debug" set debug=true
 
 if not exist %prjRoot%\bin\ (
 	echo There is nothing to run...
 ) else (
-	cd %prjRoot%\bin\
-	qemu-system-i386 -cdrom pandaOS.iso
-	cd %startingDirectory%
+	if %debug%==true (
+		bochsdbg -q -f bochsrc-debug.bxrc
+	) else (
+		cd %prjRoot%\bin\
+		qemu-system-i386 -cdrom pandaOS.iso
+		cd %startingDirectory%
+	)
 )
-
