0869aea7e1d2bf448fbd68b513435631ce8c3d7c
FancyKillerPanda <iyersahil12@gmail.com>
2020-09-13 08:46:30 +1000

    Kernel: Moving hardware cursor can be disabled.

--- src/kernel/system/display.cpp
+++ src/kernel/system/display.cpp
@@ -10,8 +10,12 @@
 #define SCREEN_COLS 80
 
 // Current cursor position
-u8 cursorX = 0;
-u8 cursorY = 0;
+u8 cursorRow = 0;
+u8 cursorCol = 0;
+
+// Whether the hardware cursor should be moved
+// This is turned off at the start of string printing
+bool shouldMoveCursor = true;
 
 void enable_cursor()
 {
@@ -28,17 +32,21 @@ void enable_cursor()
 
 void move_cursor(u8 row, u8 col)
 {
-	cursorX = col;
-	cursorY = row;
-	u16 location = (cursorY * SCREEN_COLS) + cursorX;
+	cursorCol = col;
+	cursorRow = row;
 
-	// The low byte
-	port_out_8(0x3d4, 0x0f);
-	port_out_8(0x3d5, (u8) (location & 0xff));
+	if (shouldMoveCursor)
+	{
+		u16 location = (cursorRow * SCREEN_COLS) + cursorCol;
 
-	// The high byte
-	port_out_8(0x3d4, 0x0e);
-	port_out_8(0x3d5, (u8) ((location >> 8) & 0xff));
+		// The low byte
+		port_out_8(0x3d4, 0x0f);
+		port_out_8(0x3d5, (u8) (location & 0xff));
+
+		// The high byte
+		port_out_8(0x3d4, 0x0e);
+		port_out_8(0x3d5, (u8) ((location >> 8) & 0xff));
+	}
 }
 
 void disable_cursor()
@@ -62,9 +70,9 @@ void scroll_screen_up(u8 numberOfLines, u8 attribute)
 	if (numberOfLines == 0)
 	{
 		// Scrolls enough to have one line available
-		if (cursorY >= SCREEN_ROWS)
+		if (cursorRow >= SCREEN_ROWS)
 		{
-			numberOfLines = cursorY - SCREEN_ROWS + 1;
+			numberOfLines = cursorRow - SCREEN_ROWS + 1;
 		}
 		else
 		{
@@ -74,39 +82,39 @@ void scroll_screen_up(u8 numberOfLines, u8 attribute)
 
 	copy_memory(VIDEO_MEMORY + (numberOfLines * SCREEN_COLS), VIDEO_MEMORY, SCREEN_ROWS * SCREEN_COLS * 2);
 	set_memory_16(VIDEO_MEMORY + (SCREEN_ROWS - numberOfLines) * SCREEN_COLS, SCREEN_COLS * numberOfLines, blank);
-	move_cursor(cursorY - numberOfLines, cursorX);
+	move_cursor(cursorRow - numberOfLines, cursorCol);
 }
 
 void print_char(u8 character, u8 attribute)
 {
-	u16 location = (cursorY * SCREEN_COLS) + cursorX;
+	u16 location = (cursorRow * SCREEN_COLS) + cursorCol;
 
 	switch (character)
 	{
 	case '\b':
 	{
-		if (cursorX > 0)
+		if (cursorCol > 0)
 		{
 			u16 blank = 0x20 | (attribute << 8);
 			*(VIDEO_MEMORY + location - 1) = blank;
-			move_cursor(cursorY, cursorX - 1);
+			move_cursor(cursorRow, cursorCol - 1);
 		}
 	} break;
 		
 	case '\r':
 	{
-		move_cursor(cursorY, 0);
+		move_cursor(cursorRow, 0);
 	} break;
 	
 	case '\n':
 	{
-		move_cursor(cursorY + 1, 0);
+		move_cursor(cursorRow + 1, 0);
 	} break;
 
 	case '\t':
 	{
 		// Cursor moves to a location divisible by four
-		move_cursor(cursorY, (cursorX + 4) & ~(4 - 1));
+		move_cursor(cursorRow, (cursorCol + 4) & ~(4 - 1));
 	} break;
 	
 	default:
@@ -116,17 +124,17 @@ void print_char(u8 character, u8 attribute)
 		{
 			u16 data = character | (attribute << 8);
 			*(VIDEO_MEMORY + location) = data;
-			move_cursor(cursorY, cursorX + 1);
+			move_cursor(cursorRow, cursorCol + 1);
 		}
 	} break;
 	}
 	
-	if (cursorX >= SCREEN_COLS)
+	if (cursorCol >= SCREEN_COLS)
 	{
-		move_cursor(cursorY + 1, 0);
+		move_cursor(cursorRow + 1, 0);
 	}
 
-	if (cursorY >= SCREEN_ROWS)
+	if (cursorRow >= SCREEN_ROWS)
 	{
 		scroll_screen_up();
 	}
@@ -134,9 +142,14 @@ void print_char(u8 character, u8 attribute)
 
 void print(const u8* string, u8 attribute)
 {
+	shouldMoveCursor = false;
+	
 	while (*string)
 	{
 		print_char(*string, attribute);
 		string += 1;
 	}
+
+	shouldMoveCursor = true;
+	move_cursor(cursorRow, cursorCol);
 }
