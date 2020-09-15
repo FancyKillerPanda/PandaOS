e5d3d28842b63f89bf85a79d3938f520170bea43
FancyKillerPanda <iyersahil12@gmail.com>
2020-09-15 17:17:07 +1000

    genVDisk: VBR and space for the FATs are written to the extent file.

--- src/genVDisk/genVDisk.cpp
+++ src/genVDisk/genVDisk.cpp
@@ -8,7 +8,18 @@
 #include "clargs.hpp"
 #include "geometry.hpp"
 
-bool write_descriptor_file(u8* descriptorFileName, usize hardDiskSize);
+bool write_descriptor_file(const u8* descriptorFileName, usize hardDiskSize);
+bool write_extent_file(const u8* extentFileName, const CLArgs& arguments);
+bool write_data_as_blocks(FILE* file, const u8* data, usize size, usize minNumberOfBlocks = 0);
+u16 read_word(const u8* data, usize indexOfFirstByte);
+
+struct FAT16Information
+{
+	u16 bytesPerSector = 0;
+	u8 fatCount = 0;
+	u8 mediaDescriptor = 0;
+	u16 sectorsPerFat = 0;
+};
 
 i32 main(i32 argc, const u8* argv[])
 {
@@ -19,23 +30,25 @@ i32 main(i32 argc, const u8* argv[])
 		return 1;
 	}
 
-	// Generates the file names
+	// The descriptor file
 	usize imageNameLength = strlen(arguments.imageName);
-	u8* descriptorFileName = (u8*) malloc(imageNameLength + strlen(".vmdk") + 1);
+	u8* descriptorFileName = (u8*) calloc(imageNameLength + strlen(".vmdk") + 1, sizeof(u8));
 	memcpy(descriptorFileName, arguments.imageName, imageNameLength);
 	strcat(descriptorFileName, ".vmdk");
 	
 	write_descriptor_file(descriptorFileName, 64 * 1024 * 1024);
 	free(descriptorFileName);
 	
-	u8* extentFileName = (u8*) malloc(imageNameLength + strlen("-flat.vmdk") + 1);
+	// The extent file
+	u8* extentFileName = (u8*) calloc(imageNameLength + strlen("-flat.vmdk") + 1, sizeof(u8));
 	memcpy(extentFileName, arguments.imageName, imageNameLength);
 	strcat(extentFileName, "-flat.vmdk");
 
+	write_extent_file(extentFileName, arguments);
 	free(extentFileName);
 }
 
-bool write_descriptor_file(u8* descriptorFileName, usize hardDiskSize)
+bool write_descriptor_file(const u8* descriptorFileName, usize hardDiskSize)
 {
 	DiskGeometry diskGeometry;
 	
@@ -44,7 +57,7 @@ bool write_descriptor_file(u8* descriptorFileName, usize hardDiskSize)
 		return false;
 	}
 	
-	FILE* descriptorFile = fopen(descriptorFileName, "w");
+	FILE* descriptorFile = fopen(descriptorFileName, "wb");
 
 	if (!descriptorFile)
 	{
@@ -83,4 +96,91 @@ bool write_descriptor_file(u8* descriptorFileName, usize hardDiskSize)
 
 	fclose(descriptorFile);
 	return true;
+}
+
+bool write_extent_file(const u8* extentFileName, const CLArgs& arguments)
+{
+	FILE* extentFile = fopen(extentFileName, "wb");
+
+	if (!extentFile)
+	{
+		printf("Error: Failed to open extent file (%s).\n", extentFileName);
+		return false;
+	}
+
+	FILE* vbrFile = fopen(arguments.volumeBootRecordFile, "rb");
+
+	if (!vbrFile)
+	{
+		printf("Error: VBR file (%s) is invalid.\n", arguments.volumeBootRecordFile);
+		fclose(extentFile);
+		return false;
+	}
+	
+	// Reads the entire contents of the VBR file and writes it to the extent file
+	fseek(vbrFile, 0, SEEK_END);
+	usize vbrFileSize = ftell(vbrFile);
+	fseek(vbrFile, 0, SEEK_SET);
+
+	if (vbrFileSize != 512)
+	{
+		printf("VBR file (%s) is not 512 bytes long.\n", arguments.volumeBootRecordFile);
+		fclose(vbrFile);
+		fclose(extentFile);
+		
+		return false;
+	}
+	
+	u8* vbrFileContents = (u8*) malloc(vbrFileSize);
+	fread(vbrFileContents, 1, vbrFileSize, vbrFile);
+	write_data_as_blocks(extentFile, vbrFileContents, vbrFileSize);
+
+	// Gets information about the FAT from the BPB/EBPB in the VBR
+	FAT16Information fat16Information;
+	fat16Information.bytesPerSector = read_word(vbrFileContents, 0x0b);
+	fat16Information.fatCount = vbrFileContents[0x10];
+	fat16Information.mediaDescriptor = vbrFileContents[0x15];
+	fat16Information.sectorsPerFat = read_word(vbrFileContents, 0x16);
+	free(vbrFileContents);
+	fclose(vbrFile);
+
+	// Writes out the FAT table
+	for (u8 i = 0; i < fat16Information.fatCount; i++)
+	{
+		write_data_as_blocks(extentFile, data, 4, fat16Information.sectorsPerFat);
+	}
+
+	fclose(extentFile);
+	return true;
+}
+
+bool write_data_as_blocks(FILE* file, const u8* data, usize size, usize minNumberOfBlocks)
+{
+	constexpr usize blockSize = 512;
+	usize amountOfPadding;
+
+	if (blockSize * minNumberOfBlocks <= size)
+	{
+		amountOfPadding = (blockSize - (size % blockSize)) % blockSize;
+	}
+	else
+	{
+		amountOfPadding = (blockSize * minNumberOfBlocks) - size;
+	}
+	
+	u8* padding = (u8*) calloc(amountOfPadding, sizeof(u8));
+	
+	fwrite(data, 1, size, file);
+	fwrite(padding, 1, amountOfPadding, file);
+	
+	return true;
+}
+
+u16 read_word(const u8* data, usize indexOfFirstByte)
+{
+	u16 firstByte = (u16) data[indexOfFirstByte];
+	u16 secondByte = (u16) data[indexOfFirstByte + 1];
+	u16 result = (secondByte << 8) | firstByte;
+
+	return result;
 }
