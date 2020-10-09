98d0ad9aafe577af4c0adad4401597d44da91935
FancyKillerPanda <iyersahil12@gmail.com>
2020-10-09 11:56:36 +1100

    Project: Enabled Bochs' magic breakpoints and port e9 hack.

--- tools/scripts/run.bat
+++ tools/scripts/run.bat
@@ -4,14 +4,25 @@ rem  ===== Date Created: 03 September, 2020 =====
 set scriptDir=%~dp0
 set prjRoot=%scriptDir%\..\..
 set debug=false
+set gui=false
 
-if "%~1" == "-debug" set debug=true
+for %%a in (%*) do (
+	if [%%a]==[-debug] (
+		set debug=true
+	) else if [%%a]==[-gui] (
+		set gui=true
+	)
+)
 
 if not exist %prjRoot%\bin\PandaOS\ (
 	echo There is nothing to run...
 ) else (
 	if %debug%==true (
-		bochsdbg -q -f %prjRoot%\tools\debug\bochsrc-debug.bxrc
+		if %gui%==true (
+			bochsdbg -q -f %prjRoot%\tools\debug\bochsrc-debug.bxrc
+		) else (
+			bochsdbg -q -f %prjRoot%\tools\debug\bochsrc.bxrc
+		)
 	) else (
 		pushd %prjRoot%\bin\PandaOS\
 		qemu-system-i386 -drive file=PandaHDD.vmdk,index=0,media=disk -debugcon stdio -serial file:pandaLog.txt
