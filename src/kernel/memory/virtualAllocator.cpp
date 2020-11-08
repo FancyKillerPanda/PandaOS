fb03584c1773ddcdb9293e83107f82855c835ccb
FancyKillerPanda <iyersahil12@gmail.com>
2020-11-08 21:35:16 +1100

    Kernel: Added using definitions for page table family of types.

--- src/kernel/memory/virtualAllocator.cpp
+++ src/kernel/memory/virtualAllocator.cpp
@@ -5,38 +5,42 @@
 #include "display.hpp"
 #include "physicalAllocator.hpp"
 
+using PageTableEntry = u32;
+using PageTable = PageTableEntry*;
+using PageDirectory = PageTable*;
+
 constexpr u32 NUMBER_OF_PAGE_DIRECTORY_ENTRIES = 1024;
 constexpr u32 NUMBER_OF_PAGE_TABLE_ENTRIES = 1024;
 constexpr u32 KERNEL_FLAT_ADDRESS = 0x20000;
-u32* pageDirectory = nullptr;
+PageDirectory pageDirectory = nullptr;
 
-extern "C" void load_page_directory(u32* pageDirectory);
+constexpr u32 PRESENT_FLAG = 0x01;
+constexpr u32 READ_WRITE_FLAG = 0x02;
+
+extern "C" void load_page_directory(PageDirectory pageDirectory);
 extern "C" void enable_paging();
 
 void init_virtual_allocator()
 {
-	constexpr u32 presentFlag = 0x01;
-	constexpr u32 readWriteFlag = 0x02;
-	
 	// Initialises an unmapped page directory
-	pageDirectory = (u32*) allocate_physical_page();
+	pageDirectory = (PageDirectory) allocate_physical_page();
 
 	for (u32 i = 0; i < NUMBER_OF_PAGE_DIRECTORY_ENTRIES; i++)
 	{
-		pageDirectory[i] = readWriteFlag;
+		pageDirectory[i] = (PageTable) READ_WRITE_FLAG;
 	}
 	
 	// Initialises the first page table
-	u32* firstPageTable = (u32*) allocate_physical_page();
+	PageTable firstPageTable = (PageTable) allocate_physical_page();
 
 	for (u32 i = 0; i < NUMBER_OF_PAGE_TABLE_ENTRIES; i++)
 	{
-		firstPageTable[i] = (i * PAGE_SIZE) | presentFlag | readWriteFlag;
+		firstPageTable[i] = (i * PAGE_SIZE) | PRESENT_FLAG | READ_WRITE_FLAG;
 	}
 
 	// Puts the first page table into the page directory and tells the
 	// processor to load it
-	pageDirectory[0] = ((u32) firstPageTable) | presentFlag | readWriteFlag;
+	pageDirectory[0] = (PageTable) (((u32) firstPageTable) | PRESENT_FLAG | READ_WRITE_FLAG);
 	load_page_directory(pageDirectory);
 	enable_paging();
 	
