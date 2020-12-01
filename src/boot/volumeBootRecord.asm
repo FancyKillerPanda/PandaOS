1d4aada74c87a50dfdc026b868c3d88d4994f748
FancyKillerPanda <iyersahil12@gmail.com>
2020-12-01 16:09:58 +1100

    Boot: Added a small print utility.
    The function takes a string in ds:si and prints it out. TODO: wrap
    this function in a macro for ease of use.

--- src/boot/volumeBootRecord.asm
+++ src/boot/volumeBootRecord.asm
@@ -1,14 +1,39 @@
 ; ===== Date Created: 01 December, 2020 ===== 
 org 0x7c00
 
+CR: equ 0x0d
+LF: equ 0x0a
+
 start:
 	jmp short main
 	nop
 
-biosParameterBlock:
-
 main:
-	
+	mov si, testString
+	call print_string
+
+	jmp $
+
+; void print_string(ds:si string)
+; This function will mangle si
+print_string:
+	.print_char:
+		mov al, [ds:si]
+		cmp al, 0
+		je .done
+
+		mov ah, 0x0e
+		mov bh, 0
+		int 0x10
+
+		inc si
+		jmp .print_char
+
+	.done:
+		ret
+
+; Data
+testString: db "Hello, world!", CR, LF, 0
 
 end:
 	times 510 - ($ - $$) db 0
