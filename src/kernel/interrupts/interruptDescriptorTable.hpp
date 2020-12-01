536f910b14f0b635f291c155a7ddc90c80f4c9b1
FancyKillerPanda <iyersahil12@gmail.com>
2020-12-01 14:29:32 +1100

    Project: A fresh beginning...

--- src/kernel/interrupts/interruptDescriptorTable.hpp
+++
@@ -1,8 +0,0 @@
-//  ===== Date Created: 24 September, 2020 ===== 
-
-#if !defined(INTERRUPTDESCRIPTORTABLE_HPP)
-#define INTERRUPTDESCRIPTORTABLE_HPP
-
-void init_interrupt_descriptor_table();
-
-#endif
