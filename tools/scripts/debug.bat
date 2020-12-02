90b5bf689a1edd0b2ad278922cb42612880d2a85
FancyKillerPanda <iyersahil12@gmail.com>
2020-12-02 18:15:55 +1100

    Project: Added a Bochs debug script.
    This has to be run on windows, unfortunately.

---
+++ tools/scripts/debug.bat
@@ -0,0 +1,11 @@
+@echo off
+rem  ===== Date Created: 02 December, 2020 ===== 
+
+set scriptDir=%~dp0
+set prjRoot=%scriptDir%\..\..
+
+if not exist %prjRoot%\bin\PandaOS\ (
+	echo There is nothing to run...
+) else (
+	bochsdbg -q -f %prjRoot%\tools\debug\bochsrc.bxrc
+)
